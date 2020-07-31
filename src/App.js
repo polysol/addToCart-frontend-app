5fa1658e077f439d2346fa2489d2bd288821dec4
ppapagianni <ppapagianni@knowledge.gr>
2020-07-31 16:40:48 +0300

    app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Container from "./Container";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            marginLeft: theme.spacing(5),
+            marginTop: theme.spacing(3),
+            backgroundColor: '#2d88ff',
+            height: '50px',
+            color: 'white',
+            width: '250px'
+        },
+        '& > *:hover' : {
+            backgroundColor: '#3A3B3C',
+        },
+        textAlign: 'center',
+        border: '1px'
+    },
+    search: {
+        backgroundColor: 'red',
+    },
+    cart: {
+        backgroundColor: 'green',
+    }
+}));
 
 function App() {
+    const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+        <Button variant="outlined" onClick={<Container />}>Πουλησε Αντικειμενο</Button>
+        <Button variant="outlined" className={classes.search}>Αναζητησε Αντικειμενο</Button>
+        <Button variant="outlined" className={classes.cart}>Καλαθι Αγορων</Button>
     </div>
   );
 }
---
+++ src/Container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function Container() {
+    return (
+                <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+    );
+}
+
+export default Container;
