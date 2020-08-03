8671fe67b6a8e563c2372bf46cd0a74ac6205d87
psolomou <psolomou@knowledge.gr>
2020-08-03 23:35:51 +0300

    add product form

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
 
 function AddProduct() {
     return (
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: 'calc(100vh - 10em)', width:'calc(100vw - 5em)' }} />
-
+        <div style={{ backgroundColor: '#cfe8fc', height: 'calc(100vh - 10em)', width:'calc(100vw - 5em)' }} >
+            <br></br>
+        <FormControl variant="outlined">
+            <InputLabel htmlFor="component-outlined">Name</InputLabel>
+            <OutlinedInput id="component-outlined" label="Name" />
+        </FormControl>
+        </div>
     );
 }
 
