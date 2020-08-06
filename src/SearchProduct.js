88f6252a282286407703cf620737e8da8911363f
ppapagianni <ppapagianni@knowledge.gr>
2020-08-06 17:04:30 +0300

    editable rows

--- src/SearchProduct.js
+++ src/SearchProduct.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import MUIDataTable from "mui-datatables";
 import Tooltip from "@material-ui/core/Tooltip";
 import AddIcon from '@material-ui/icons/Add';
+import SaveIcon from '@material-ui/icons/Save';
 import IconButton from "@material-ui/core/IconButton";
 import {createMuiTheme,MuiThemeProvider} from '@material-ui/core/styles';
 import { TableCell, TableRow } from "@material-ui/core";
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
 
 function SearchProduct() {
     const getMuiTheme = () => createMuiTheme({
@@ -25,49 +28,124 @@ function SearchProduct() {
                     fontSize: '16px'
                 }
             },
+            MuiInputBase: {
+                input: {
+                    background: "none",
+                    fontSize: '0.875rem',
+                    padding: '6px 10px 7px 10px',
+                    border: '1px black solid'
+                }
+            },
         }
     });
 
-    const columns = ["Προϊόν", "Τιμή", "Email Πωλητή", "Πόλη"];
+    const columns = [
+        {
+        label: "Προϊόν",
+        options: {
+            customBodyRender: (value, tableMeta, updateValue) => {
+                return (
+                    <FormControlLabel
+                        required
+                        value={value}
+                        control={<TextField value={value} />}
+                        onChange={event => updateValue(event.target.value)}
+                    />
+                )
+            }
+        }
+    },
+        {
+            label: "Περιγραφή",
+            options: {
+                customBodyRender: (value, tableMeta, updateValue) => {
+                    return (
+                        <FormControlLabel
+                            required
+                            value={value}
+                            control={<TextField value={value} />}
+                            onChange={event => updateValue(event.target.value)}
+                        />
+                    )
+                }
+            }
+        },
+        {
+        label: "Τιμή",
+        options: {
+            customBodyRender: (value, tableMeta, updateValue) => {
+                return (
+                    <FormControlLabel
+                        required
+                        value={value}
+                        control={<TextField value={value} />}
+                        onChange={event => updateValue(event.target.value)}
+                    />
+                )
+            }
+        }
+    }, {
+        label: "Email Πωλητή",
+        options: {
+        customBodyRender: (value, tableMeta, updateValue) => {
+            return (
+                <FormControlLabel
+                    required
+                    value={value}
+                    control={<TextField value={value} />}
+                    onChange={event => updateValue(event.target.value)}
+                />
+            )
+        }
+    }
+}, {
+        label: "Πόλη",
+            options: {
+            customBodyRender: (value, tableMeta, updateValue) => {
+                return (
+                    <FormControlLabel
+                        required
+                        value={value}
+                        control={<TextField value={value} />}
+                        onChange={event => updateValue(event.target.value)}
+                    />
+                )
+            }
+        }
+    }
+    ];
 
     const options = {
-        expandableRows: true,
-        expandableRowsHeader: false,
         filter: false,
-        selectableRows: 'single' || 'multiple',
+        selectableRows: 'single',
         selectableRowsOnClick: true,
-        renderExpandableRow: () => (
-            <TableRow>
-                <TableCell/>
-                <TableCell>
-                    Σε καλή κατάσταση. Ελαφρώς μεταχειρισμένος.
-                </TableCell>
-            </TableRow>
-        ),
         onRowsSelect: function(row) {
         },
         download: false,
         print: false,
         viewColumns: false,
         customToolbarSelect: selectedRows => (
+            <React.Fragment>
             <Tooltip title="Προσθήκη στο καλάθι">
                 <IconButton
                     onClick={() => {
-                        console.log("test");
+                        console.log("test1");
 
-                    }}
-                    style={{
-                        marginRight: "24px",
-                        height: "48px",
-                        top: "15px",
-                        display: "block",
-                        position: "relative",
-                        transform: "translateY(-50%)"
                     }}
                 >
                     <AddIcon />
                 </IconButton>
             </Tooltip>
+            <Tooltip title="Αποθήκευση αλλαγών">
+        <IconButton
+        onClick={() => {
+            console.log("test2");
+    }}
+>
+<SaveIcon />
+    </IconButton>
+</Tooltip>
+</React.Fragment>
         ),
         rowsPerPageOptions:  [] ,
         textLabels: {
@@ -84,7 +162,8 @@ function SearchProduct() {
 
 
     const data = [
-        ["Ανεμιστήρας", "15.00$", "aggelo@gmail.com", "Πάτρα"]
+        ["Ανεμιστήρας", "Σε πολύ καλή κατάσταση","15.00$", "aggelo@gmail.com", "Πάτρα"],
+        ["Ψυγείο", "Αγοράστηκε πριν 2 χρόνια και πωλείται λόγω αγοράς καινούριου","230.00$", "coaler@gmail.com", "Αθήνα"]
     ];
     return (
         <div style={{ backgroundColor: '#ffffff', height: 'calc(100vh - 10em)', width:'calc(100vw - 5em)'}} >
